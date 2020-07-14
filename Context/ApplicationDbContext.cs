1be3e1fec5b806737329b3db622e70f117731f0c
Eduardo Cabrera <r.eduardo.cabrera@hotmail.com>
2020-07-13 22:58:45 -0600

    Start project

---
+++ Context/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using JobBoardModels.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JobBoardModels.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        { 
+        
+        }
+
+        public DbSet<Job> Jobs { get; set; }
+    }
+}
---
+++ Entities/Job.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace JobBoardModels.Entities
+{
+    public class Job
+    {
+        public int JobId { get; set; }
+        [Required]
+        public string JobTitle { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public DateTime CreatedAt { get; set; }
+        [Required]
+        public DateTime ExpiresAt { get; set; }
+    }
+}
